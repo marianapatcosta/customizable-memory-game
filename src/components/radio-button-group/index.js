edd44a522bee6564a6367d7f0a2daec2247012e1
marianapatc <marianapatc@hotmail.com>
2021-01-09 19:17:00 +0000

    develop radio-button and radio-button-group components

--- src/components/radio-button-group/index.js
+++ src/components/radio-button-group/index.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { RadioButton } from "..";
+import { orientation } from "../../constants";
 import "./RadioButtonGroup.css";
 
-const RadioButtonGroup = ({  }) => (
-  <span
-  >
-    
-  </span>
-);
+const RadioButtonGroup = ({ disabled, display, radioButtonMetadata }) => {
+  const [activeRadioButtonIndex, setActiveRadioButtonIndex] = useState(0);
+
+  const onClickRadioButton = (radioButtonIndex) =>
+    setActiveRadioButtonIndex(radioButtonIndex);
+
+  return (
+    <div className={`radio-button-group ${display === orientation[1] ? 'radio-button-group--landscape' : ''}`}>
+      {radioButtonMetadata.map((data, index) => (
+        <RadioButton
+          key={index * Math.random()}
+          isActive={activeRadioButtonIndex === index}
+          disabled={disabled}
+          label={data}
+          display={display}
+          onClick={() => onClickRadioButton(index)}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default RadioButtonGroup;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import GameBoard from "./game-board";
 import GameOver from "./game-over";
+import GameSetup from "./game-setup";
 import GameStart from "./game-start";
 
-export { GameBoard, GameOver, GameStart };
+export { GameBoard, GameOver, GameSetup, GameStart };
