c6fe08c8a9678bbd246011e1f59870c175a9f54d
marianapatc <marianapatc@hotmail.com>
2021-01-12 21:45:55 +0000

    add MenuBar component

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,7 @@
 import Chevron from "./chevron.svg";
+import Delete from "./delete.svg";
+import Maximize from "./maximize.svg";
+import Minimize from "./minimize.svg";
+import Restore from "./restore.svg";
 
-export { Chevron };
+export { Chevron, Delete, Maximize, Minimize, Restore };
---
+++ src/components/menu-bar/index.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Delete, Maximize, Minimize, Restore } from "../../assets/icons";
+import { isElectron } from "../../utils";
+import "./MenuBar.css";
+const isElectronProcess = isElectron();
+const electron = isElectronProcess && window.require("electron");
+const remote = isElectronProcess && electron.remote;
+
+const Menubar = ({title}) => {
+  const [isWindowMaximized, setIsWindowMaximized] = useState(false);
+
+  const handleQuit = () => {
+    const window = remote.getCurrentWindow();
+    window.close();
+  };
+
+  const handleMinimize = () => {
+    const window = remote.getCurrentWindow();
+    window.minimize();
+  };
+
+  const handleMaximize = () => {
+    const window = remote.getCurrentWindow();
+    window.maximize()
+    setIsWindowMaximized(true);
+  };
+
+  const handleRestore = () => {
+    const window = remote.getCurrentWindow();
+    window.restore();
+    setIsWindowMaximized(false);
+  };
+
+  return (
+    <div className="menu-bar">
+    <div className="menu-bar__title">{title}</div>
+      <div className="menu-bar__icons">
+        <img
+          className="menu-bar__icon"
+          alt="minimize icon"
+          src={Minimize}
+          onClick={handleMinimize}
+        />
+        {isWindowMaximized ? (
+          <img
+            className="menu-bar__icon"
+            alt="restore icon"
+            src={Restore}
+            onClick={handleRestore}
+          />
+        ) : (
+          <img
+            className="menu-bar__icon"
+            alt="maximize icon"
+            src={Maximize}
+            onClick={handleMaximize}
+          />
+        )}
+        <img
+          className="menu-bar__icon menu-bar__icon--red-hover"
+          alt="delete icon"
+          src={Delete}
+          onClick={handleQuit}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Menubar;
