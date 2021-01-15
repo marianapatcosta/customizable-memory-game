71a483c3909c201f325e84033addbc84b3197ba0
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2021-01-15 21:30:30 +0000

    Implement responsive design

--- src/pages/game-over/index.js
+++ src/pages/game-over/index.js
@@ -10,40 +10,63 @@ const GameOver = ({ restartGame }) => {
     <div className="game-over">
       <Confetti />
       <h1 className="game-over__title">
-        CONGRATULATIONS! <br /> You won!
+        Congratulations! <br /> You won!
       </h1>
-      <div className="game-over__balloons">
-        <div className="game-over__balloon">
-          <span className="game-over__balloon--number">?</span>
+      <div className="game-over__top">
+        <div className="game-over__balloons">
+          <div className="game-over__balloon">
+            <span className="game-over__balloon--number">?</span>
+          </div>
+          <div className="game-over__balloon">
+            <span className="game-over__balloon--number">?</span>
+          </div>
+          <div className="game-over__balloon">
+            <div className="game-over__balloon--image">
+              <Emoji label="party" emoji="🥳" />
+            </div>
+            {/*  <img
+              className="game-over__balloon--image"
+              src="./0.jpg"
+              alt="balloon"
+            /> */}
+          </div>
         </div>
-        <div className="game-over__balloon">
-          <span className="game-over__balloon--number">?</span>
-        </div>
-        <div className="game-over__balloon game-over__balloon--image">
-          <Emoji label="party" emoji="🥳" />
-          {/*  <img
-            className="game-over__balloon--image"
-            src="./0.jpg"
-            alt="balloon"
-          /> */}
-        </div>
-      </div>
-      <img
-        className="game-over__party-hat"
-        src="./party-hat.png"
-        alt="party hat"
-      />
-      <div className="game-over__carousel">
-        <Carousel
-          header="Carousel Photos"
-          items={photos}
-          /* hasPreviews={true}*/ landscape={true}
+        <img
+          className="game-over__party-hat"
+          src="./party-hat.png"
+          alt="party hat"
         />
       </div>
-      <Button onClick={restartGame} label="Play Again" />
-      <audio controls autoPlay loop>
-        <source src="./parabens.mp3" type="audio/mpeg"></source>
-      </audio>
+      <div className="game-over__body">
+        {window.matchMedia("(max-width: 768px)").matches && (
+          <div className="game-over__carousel">
+            <Carousel
+              header="Carousel Photos"
+              items={photos}
+              landscape={true}
+            />
+          </div>
+        )}
+        <div className="game-over__body--bottom">
+          <audio controls autoPlay loop>
+            <source src="./parabens.mp3" type="audio/mpeg"></source>
+          </audio>
+          <Button
+            className="game-over__button"
+            onClick={restartGame}
+            label="Play Again"
+          />
+        </div>
+        {window.matchMedia("(min-width: 767px)").matches && (
+          <div className="game-over__carousel">
+            <Carousel
+              header="Carousel Photos"
+              items={photos}
+              /* hasPreviews={true}*/ landscape={true}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
